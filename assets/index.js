be7975a955038a572ddf7fd84cd3f93ef54c6972
teslasdev <Teslasdeveloper@gmail.com>
2023-10-26 02:25:32 +0100

    upload asset

--- assets/index.js
+++ assets/index.js
@@ -11,6 +11,15 @@ import telegram from './telegram.png';
 import twitter from './twitter.png';
 import upload from './upload.png';
 import creator1 from './creator1.png';
+import creator2 from './creator2.png';
+import creator3 from './creator3.png';
+import creator4 from './creator4.png';
+import creator5 from './creator5.png';
+import creator6 from './creator6.png';
+import creator7 from './creator7.png';
+import creator8 from './creator8.png';
+import creator9 from './creator9.png';
+import creator10 from './creator10.jpg';
 import right from './right.png';
 import left from './left.png';
 import loader from './loader.gif';
@@ -32,6 +41,15 @@ export default {
     twitter,
     upload,
     creator1,
+    creator2,
+    creator3,
+    creator4,
+    creator5,
+    creator6,
+    creator7,
+    creator8,
+    creator9,
+    creator10,
     right,
     left,
     loader,
